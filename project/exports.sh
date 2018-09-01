cde5f9541b03be148a6502429da5d6c59754cff7
Giovanni Gaspar <giovannigaspar@outlook.com>
2018-09-01 11:26:54 -0300

    Instruções e tabela do banco de dados

---
+++ project/exports.sh
@@ -0,0 +1,8 @@
+# Dados que serão utilizados pelo banco de dados.
+export DATABASE_NAME='desafio_4all'
+export DATABASE_USER='sysdba'
+export DATABASE_PASSWORD='masterkey'
+
+# Remove os arquivos "pycache", assim não gera bagunça no ambiente de 
+# desenvolvimento
+export PYTHONDONTWRITEBYTECODE=1 
