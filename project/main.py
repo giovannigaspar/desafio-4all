1219b7549d1ff797ded2155306a1bbc1f633652a
Giovanni Gaspar <giovannigaspar@outlook.com>
2018-09-02 11:47:00 -0300

    Aplicação funcional - Armazenando dados no banco de dados

--- project/main.py
+++ project/main.py
@@ -1,5 +1,8 @@
+from db import get_dict_resultset, ONE
+
 # Biblioteca de geolocalização open source: https://github.com/geopy/geopy
 from geopy.geocoders import Nominatim
+
 
 # Inicializando a biblioteca (sintaxe padrão)
 geolocator = Nominatim(user_agent="desafio_4all")
@@ -18,9 +21,9 @@ def read_coordinates_file(filename, debug=False):
     como forma de debug
 
 
-    :return: Dicionário contendo as informações no formato 
-    [{'lat':'y', 'long':'x'}]
-    """    
+    :return: Dicionário contendo as informações no formato
+    [{'lat':'y', 'lon':'x'}]
+    """
     coordinates = []
     with open(filename, 'r', newline='') as f:
         content = f.readlines()
@@ -28,7 +31,7 @@ def read_coordinates_file(filename, debug=False):
             if (idx+1) % 3 == 0:
                 coordinates.append({
                     'lat': content[idx-2].split('   ')[1].rstrip('\n'),
-                    'long': content[idx-1].split('   ')[1].rstrip('\n')
+                    'lon': content[idx-1].split('   ')[1].rstrip('\n')
                 })
     if debug:
         print(coordinates)
@@ -37,19 +40,19 @@ def read_coordinates_file(filename, debug=False):
 
 def get_locations(coordinates, debug=False):
     """
-    Obtém informações de um local (endereço, CEP, bairro, etc) a partir de 
+    Obtém informações de um local (endereço, CEP, bairro, etc) a partir de
     coordenadas (latitude e longitude) geográficas.
 
 
     :param coordinates: coordenadas a serem verificadas. Espera-se que estejam
-    no padrão [{'lat':'y', 'long':'x'}]
+    no padrão [{'lat':'y', 'lon':'x'}]
 
-    :param debug: utilizado para imprimir as informações do local como forma de 
+    :param debug: utilizado para imprimir as informações do local como forma de
     debug
 
     :return: JSON contendo as informações do local pesquisado
     """
-    points = coordinates['lat'] + ', ' + coordinates['long']
+    points = coordinates['lat'] + ', ' + coordinates['lon']
     location = geolocator.reverse(points)
     if debug:
         print(location.raw)
@@ -57,20 +60,48 @@ def get_locations(coordinates, debug=False):
 
 
 
-def populate_database(info):
+def populate_database(js):
     """
     Armazena informações no banco de dados a partir de um JSON.
 
-    :param info: JSON contendo as informações a serem armazenadas.
+    :param js: JSON contendo as informações a serem armazenadas.
     """
-    print(info)
+    sql = '''
+        INSERT INTO RESULTADOS (
+            latitude,
+            longitude,
+            rua,
+            numero, 
+            bairro,
+            cidade,
+            cep,
+            estado,
+            pais,
+            endereco
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        RETURNING ID
+    '''
+
+    js_address = js['address']
+    param = (
+        js['lat'],
+        js['lon'],
+        js_address['road'],
+        js_address['house_number'],
+        js_address['suburb'],
+        js_address['city'],
+        js_address['postcode'],
+        js_address['state'],
+        js_address['country'],
+        js['display_name']
+    )
+    return get_dict_resultset(sql, param, ONE)
 
 
 def main():
     # Leitura do arquivo de coordenadas
-    coordinates = read_coordinates_file('tests/data_points_20180101.txt', True)
-
-    populate_database(get_locations(coordinates[0]))
+    coordinates = read_coordinates_file('tests/data_points_20180101.txt')
+    print(populate_database(get_locations(coordinates[0])))
 
 
 main()
