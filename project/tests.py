83c8c508aaafdd626fe9a4ae6549a6451028a5f9
Giovanni Gaspar <giovannigaspar@outlook.com>
2018-09-03 19:24:40 -0300

    Ajuste nos testes

--- project/tests.py
+++ project/tests.py
@@ -1,11 +1,12 @@
 import unittest
+import json
 from app import main_route
 from db import get_dict_resultset, ALL
 
 
 class MyTest(unittest.TestCase):
     def test_01_insert(self):
-        r = main_route('tests/test1.txt')# + main_route('tests/test2.txt')
+        r = main_route('tests/test1.txt') + main_route('tests/test2.txt')
         print('\n\n'+str(r)+' itens inseridos no banco de dados!')
 
     def test_02_select(self):
@@ -13,9 +14,10 @@ class MyTest(unittest.TestCase):
             SELECT * FROM RESULTADOS
         '''
         r = get_dict_resultset(sql, None, ALL)
-        f = open("log.txt", "w")
-        print('\n\nAbra o arquivo "log.txt" para verificar os dados inseridos!')
-        f.write(str(r))
+        f = open("log.json", "w")
+        print('\n\nAbra o arquivo "log.json" em '+
+                'um navegador para verificar os dados inseridos!')
+        f.write(json.dumps(r))
 
     def test_03_delete(self):
         sql = '''
